2c00fcafeb7282327ae8e332b9b51d446148ea75
Matias Carrasco <mbmcarrasco@gmail.com>
2024-11-27 23:09:24 -0300

    conexion bd relacional y no relacional, limpieza de codigo

--- cocina/app/Models/User.php
+++ cocina/app/Models/User.php
@@ -6,14 +6,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use MongoDB\Laravel\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
+
     protected $collection = 'users';
-    protected $connection = 'mongodb';
     /**
      * The attributes that are mass assignable.
      *
